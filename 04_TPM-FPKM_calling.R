5210041ac49239024ccc2c0dce2a4dfd6582b509
Bethany Moore <bmjohns43@gmail.com>
2022-08-23 14:54:53 -0500

    add/update R scripts

--- 04_TPM-FPKM_calling.R
+++ 04_TPM-FPKM_calling.R
@@ -14,44 +14,80 @@ args = commandArgs(TRUE)
 RC_file <- args[1] # your read counts file
 TL_file <- args[2] # transcript length file
 
-#RC_file <- "HTSeqCount_9-month-old-internode-2_sam3_sorted_quality_60_unique.sam.out"
-#TL_file <- "Jascendensv1.1.gene.gff3.transcript_length.txt"
+RC_file <- "Read_counts.txt"
+TL_file <- "Jascendensv1.1.gene.gff3.transcript_length.txt"
 
-Exp <- read.table(RC_file,head=F,sep='\t',stringsAsFactors=F,row.names=1)
+Exp <- read.table(RC_file,head=T,sep='\t',stringsAsFactors=F,row.names=1)
 dat <- read.table(TL_file,head=T,sep='\t',stringsAsFactors=F,row.names=1)
 #Expression <- cbind(Exp[order(row.names(Exp)),],'Length'=dat[order(row.names(dat)),])
 #Expression <- as.data.frame(Expression)
 Expression <- merge(Exp, dat,
                           by = 'row.names', all = TRUE)
-Expression <- Expression[6:29125,]
+#Expression <- Expression[6:29125,]
 #samp2 <- Expression[,-1]
 #rownames(samp2) <- Expression[,1]
 Len <- Expression$transcript_length
 tpm_table <- calculateTPM(as.matrix(Expression[,2:(ncol(Expression)-1)]), Len)
-rownames(tpm_table) <- Expression[,1]
+tpm_table <- cbind(gene=Expression[,1],tpm_table)
+#rownames(tpm_table) <- Expression[,1]
 #write.table(tpm_table,'HTSeqCount_9-month-old-internode-2_sam3_sorted_quality_60_unique.sam.out_TPM.txt',row.names=T,sep='\t',quote=F)
-
+#colnames(tpm_table)
 fpkm_table <- calculateFPKM(as.matrix(Expression[,2:(ncol(Expression)-1)]), Len)
-rownames(fpkm_table) <- Expression[,1]
+#rownames(fpkm_table) <- Expression[,1]
+fpkm_table <- cbind(gene=Expression[,1],fpkm_table)
 #write.table(fpkm_table,'HTSeqCount_9-month-old-internode-2_sam3_sorted_quality_60_unique.sam.out_FPKM.txt',row.names=T,sep='\t',quote=F)
 
-out <- merge(tpm_table, fpkm_table,
-                           by = 'row.names', all = TRUE)
-
-colnames(out)<- c("gene","TPM","FPKM")
-outfile=paste(c(basename(RC_file),"_TPM-FPKM.txt"),collapse='')
-write.table(out, outfile,row.names=F,sep='\t',quote=F)
+#out <- merge(tpm_table, fpkm_table,
+#                           by = 'row.names', all = TRUE)
 
 
-###
-# tissue_list <- unique(str_remove_all(as.character(colnames(Expression)), "_\\d$"))
-# tbl_median_counts <- data.frame(row.names = rownames(tpm_table))
-# for (i in 1:length(tissue_list)) {
-#   subdat <- tpm_table[,grep(colnames(tpm_table),pattern=tissue_list[i],fixed = TRUE)]
-#   if(!is.null(nrow(subdat))){
-# 	  new_dat <- as.data.frame(rowMedians(subdat), optional = T)
-# 	  colnames(new_dat) = paste(tissue_list[i],sep='')
-# 	  tbl_median_counts <- cbind(tbl_median_counts, new_dat)
-# 	}
-# }
-# write.table(tbl_median_counts,'Median_TPM.txt',sep='\t',quote=F)
+outfile1=paste(c(basename(RC_file),"_TPM.txt"),collapse='')
+outfile2=paste(c(basename(RC_file),"_FPKM.txt"),collapse='')
+write.table(tpm_table, outfile1,row.names=F,sep='\t',quote=F)
+write.table(fpkm_table, outfile2,row.names=F,sep='\t',quote=F)
+
+
+#######################################
+#analyze reps to get median/mean values
+#######################################
+# load packages
+library(dplyr)
+install.packages("matrixStats")
+library(matrixStats)
+# read in file with column name // tissue type
+# NOTE: only use for samples that have rep- where reps have the same tissue type
+tissue_file <- "tissue-rep_list.txt"
+reps <- read.table(tissue_file,head=F,sep='\t',stringsAsFactors=F,row.names=NULL)
+tissue_list<- unique(reps$V2) # get unique tissue values
+# get median 
+tbl_median_counts_tpm <- data.frame(row.names = tpm_table[,1]) # start newtable with gene row names
+tbl_median_counts_fpkm <- data.frame(row.names = fpkm_table[,1])
+# loop through tissues to get reps, then get median of reps and add to table
+for (i in 1:length(tissue_list)) {
+  subdat<- reps %>% filter(V2 == tissue_list[i]) #get list of reps for tissue type
+  newdata1 <- tpm_table[,c(subdat$V1)] #subset reps
+  newdata2 <- fpkm_table[,c(subdat$V1)]
+  if(!is.null(ncol(subdat))){
+  mat_num1 <- matrix(as.numeric(newdata1),    # Convert to numeric matrix
+                  ncol = ncol(newdata1))
+  mat_num2 <- matrix(as.numeric(newdata2),    # Convert to numeric matrix
+                     ncol = ncol(newdata2))
+  med_dat1 <- as.data.frame(rowMedians(mat_num1),optional=T) #get median - for mean can use rowMeans
+  med_dat2 <- as.data.frame(rowMedians(mat_num2),optional=T)
+  colnames(med_dat1)= paste(tissue_list[i],sep='') # add column name
+  colnames(med_dat2)= paste(tissue_list[i],sep='')
+  tbl_median_counts_tpm <- cbind(tbl_median_counts_tpm, med_dat1) # add median to table
+  tbl_median_counts_fpkm <- cbind(tbl_median_counts_fpkm, med_dat2)
+  } 
+}
+
+# run following line if you have sample without rep, replace peduncle with name and 4 with column index
+tbl_median_counts_tpm <- cbind(peduncle=tpm_table[,4],tbl_median_counts_tpm)
+tbl_median_counts_fpkm <- cbind(peduncle=fpkm_table[,4],tbl_median_counts_fpkm)
+
+# write out median TPM
+tbl_median_counts_tpm <- cbind(gene=tpm_table[,1],tbl_median_counts_tpm)
+write.table(tbl_median_counts_tpm,'Median_TPM.txt',sep='\t',row.names=F,quote=F)
+# write out median FPKM
+tbl_median_counts_fpkm <- cbind(gene=fpkm_table[,1],tbl_median_counts_fpkm)
+write.table(tbl_median_counts_fpkm,'Median_FPKM.txt',sep='\t',row.names=F,quote=F)
---
+++ DEseq.R
@@ -0,0 +1,78 @@
+###################################
+# Differential expression (edgeR) #
+###################################
+
+# load the package and data
+# Run every time to you start a new R session.
+library(edgeR)
+
+# Download the expression data  
+counts <- read.table('counts_data.csv', header=T, row.names='gene', sep=',')
+head(counts)
+summary(counts$control_1) # Get a summary of the expression levels for the first replicate of the control treatment
+
+
+# You first need to tell edgeR where to look for your data (i.e. the genes, counts, and treatment groups) using DGEList
+# DGEList is a data container used by edgeR to organize this information
+# To define the treatment groups, you need to tell EdgeR what columns belong to the same treatment group (meaning they are replicates) 
+
+treatments <- c("C","C","C","Cold","Cold","Cold") # you are making a new list called "group" using c() of your treatments
+d <- DGEList(counts, group=treatments, genes=rownames(counts)) # rownames(counts) tells DGEList that the gene names are rownames in your counts dataframe
+
+# d is now the variable that contains your DGEList
+# You can visualize the frequency distribution of your counts in your samples using a histogram.
+# Due to the large variability in counts per gene it is useful to use the log transform counts for normalization.
+hist(d$counts, breaks = 50, xlab = "Counts")
+hist(log2(d$counts), xlim=c(0, 20), breaks=50, xlab = "LogCounts")
+
+
+# Normalize between samples
+# The goal is to normalize the counts across samples so that MOST genes have the same abundance across the different samples. 
+# The idea is that while you expect some genes will be differentially expressed across the datasets, most genes will not be affected
+# by your treatment. We can use those "unchanging" genes to normalize between the samples. 
+d <- calcNormFactors(d) #This calculates the effective library size and normalization factors. 
+d$samples 
+
+
+
+# Another way to look at the variation between your samples is to look at the "Biological Coefficient of Variation" (BCV)
+# Because of the way variance is calculated, genes with higher expression would have higher variance.
+# The BCV corrects for this by taking mean expression level into consideration. 
+# The BCV is determined by estimating the common (across all genes in all samples) and 
+# tagwise (across all samples for an individual gene) dispersal. 
+# A common BCV between 0.2-0.4 is considered good enough to be able to identify differentially expressed genes. 
+
+# Estimate the dispersion (i.e. the BCV between biological replicates).
+d <- estimateCommonDisp(d)
+d <- estimateTagwiseDisp(d)
+
+
+# Counts per million (CPM) mapped reads are counts scaled by the number of fragments you sequenced (N) times one million.
+# CPM is not scaled by the gene length. 
+# Average log CPM : average log CPM in each sample for each gene.
+
+
+
+###### Use the Fisher's Exact test to look for differentially expressed genes. #######
+
+# First we'll look for genes differentially expressed between control & cold treatment
+
+# The Fisher's exact test takes into account the "Tagwise Dispersian" when calculating the p-value
+c_vs_cold <- exactTest(d, pair=c('C','Cold')) 
+
+# Since we are calculating significance for thousands of genes, we need to use false discovery rate correction
+# Here we use the Benjamini-Hochberg (BH) method (BH) to correct the p-values, the correct p-values are called q-values.
+# We are saving the corrected p-values in a new column in the c_vs_cold$table called "FDR"
+c_vs_cold$table$FDR <- p.adjust(c_vs_cold$table$PValue, method='BH') 
+
+# Select the genes that are significantly differentially expressed (q-value <= 0.05) and have a logFC > 2 or < -2
+c_vs_cold_genes <- subset(c_vs_cold$table, c_vs_cold$table$FDR <= 0.05 & abs(c_vs_cold$table$logFC) >=2)
+
+# Check out the relationship between logFC and significance by making a "volcano" plot:
+plot(-log(c_vs_cold_genes$FDR)~c_vs_cold_genes$logFC, 
+     ylab= "logFDR",
+     xlab = "logFC") 
+
+# Save your results (you do not need to turn these in, but you may want to refer to these results later)
+write.table(c_vs_cold$table,file="Control_vs_Cold.txt",quote=FALSE,sep="\t",row.names=TRUE) #quote=FALSE, to remove the "" that are automatically added on. 
+
